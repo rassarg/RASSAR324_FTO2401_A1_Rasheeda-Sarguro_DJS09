49f33f0d402b43a537c578d6cc8004b12236c806
rasheeda <raskieda@gmail.com>
2024-06-07 16:09:55 +0200

    number types

--- index.ts
+++ index.ts
@@ -24,3 +24,11 @@ const reviews = [
     date: "27-03-2021",
   },
 ];
+
+// Solution
+function showReviewTotal(value: number) {
+  //@ts-ignore
+  reviewTotalDisplay.innerHTML = "review total " + value.toString();
+}
+
+showReviewTotal(reviews.length);
